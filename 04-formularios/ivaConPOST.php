7d2b0554314f93ebe06b23519d1601b670094bff
pma152402 <168718903+pma152402@users.noreply.github.com>
2024-10-25 14:05:24 +0200

    iva actualizado

--- 04-formularios/ivaConPOST.php
+++ 04-formularios/ivaConPOST.php
@@ -3,74 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de IVA</title>
+    <title>Document</title>
     <?php
-    //  Activamos los errores de PHP
         error_reporting( E_ALL );
-        ini_set( "display_errors", 1 );
+        ini_set("display_errors", 1 );    
 
-        require('../05_Funciones/economia.php');
+        require('../05_funciones/economia.php');
     ?>
 </head>
 <body>
-    <form action="" method="post">
-        <label for="precio">Precio</label>
-        <input type="number" name="precio" id="precio"><br><br>
-        <label for="iva">IVA</label>
-        <select name="iva" id="iva">
-            <option value="general">General</option>
-            <option value="reducido">Reducido</option>
-            <option value="superreducido">Superreducido</option>
-        </select><br><br>
-        <input type="submit" value="Calcular PVP">
-    </form>
-
-    <?php
+<?php
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $tmp_precio = $_POST["precio"];
-        $tmp_iva = $_POST["iva"];
+        if(isset($_POST["iva"])) $tmp_iva = $_POST["iva"];
+        else $tmp_iva = "";
 
-        // PRECIO
-        if ($tmp_precio != ''){
-            if (filter_var($tmp_precio, FILTER_VALIDATE_FLOAT) !== FALSE){
-                if ($tmp_precio > 0){
+        if($tmp_precio == '') {
+            $err_precio = "El precio es obligatorio";
+        } else {
+            if(filter_var($tmp_precio, FILTER_VALIDATE_FLOAT) === FALSE) {
+                $err_precio = "El precio debe ser un número";
+            } else {
+                if($tmp_precio < 0) {
+                    $err_precio = "El precio debe ser mayor o igual que cero";
+                } else {
                     $precio = $tmp_precio;
                 }
-                else {
-                    echo "<p>El precio debde de ser superior a 0</p>"; // y en verdad deberia ser el SMIP
-                }
-            }
-            else {
-                echo "<p>El precio debe de ser un numero</p>";
             }
         }
-        else {
-            echo "<p>El campo del precio no puede estar vacío</p>";
-        }
-        // IVA
-        if ($tmp_iva == ''){
-            echo "<p>El IVA es obligatorio</p>";
-        } 
-        else {
-            $valores_validos_iva = ["general", "reducido", "superreducido"];
 
-            if (!in_array($tmp_iva, $valores_validos_iva)){
-                echo "<p>El IVA solo puede ser: GENERAL, REDUCIDO o SUPERREDUCIDO";
-            }
-            else {
+        if($tmp_iva == '') {
+            $err_iva = "El IVA es obligatorio";
+        } else {
+            $valores_validos_iva = ["general", "reducido", "superreducido"];
+            if(!in_array($tmp_iva, $valores_validos_iva)) {
+                $err_iva = "El IVA solo puede ser: general, reducido, superreducido";
+            } else {
                 $iva = $tmp_iva;
             }
         }
-        var_dump($precio);
-        var_dump($iva);
-        // COMPROBACION Y OPERACION FINAL
-        if (isset($precio) && isset($iva)){
-            $resultado = calcularPVP($precio, $iva);
-            echo "<El resultado es $resultado</p>";
-        }
-       
-
     }
+    ?>
+    <form action="" method="post">
+        <label for="precio">Precio</label>
+        <input type="text" name="precio" id="precio">
+        <?php if(isset($err_precio)) echo "<span>$err_precio</span>"; ?>
+        <br><br>
+        <select name="iva">
+            <option disabled selected hidden>--- Elige un tipo de IVA ---</option>
+            <option value="general">General</option>
+            <option value="reducido">Reducido</option>
+            <option value="superreducido">Superreducido</option>
+        </select>
+        <?php if(isset($err_iva)) echo "<span>$err_iva</span>"; ?>
+        <br><br>
+        <input type="submit" value="Calcular">
+    </form>
+    <?php
+        if(isset($precio) && isset($iva)) {
+            echo "<h1>El PVP es " . calcularPVP($precio, $iva) . "</h1>";
+        }
     ?>
 </body>
 </html>
